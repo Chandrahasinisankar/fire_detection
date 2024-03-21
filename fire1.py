782321fa48e4844d1aa22663550cc8d9fcd42780
Chandrahasini <115622757+Chandrahasinisankar@users.noreply.github.com>
2024-03-21 19:37:07 +0530

    Add files via upload

---
+++ fire1.py
@@ -0,0 +1,9 @@
+import cv2
+# Read an image
+img = cv2.imread("C:\\Users\\Chandrahasini Sankar\\OneDrive\\Desktop\\chandrahasini\\project\\image_scrp\\images\\a.jpg")
+
+# Display the image
+cv2.imshow('Image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
